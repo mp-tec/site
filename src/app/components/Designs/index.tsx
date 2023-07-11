c1eb8936e47d94699e9dbda9e94f30065a45daec
Pedro Dias <71470442+diaspd@users.noreply.github.com>
2023-07-11 19:19:29 -0300

    Update index.tsx

--- src/app/components/Designs/index.tsx
+++ src/app/components/Designs/index.tsx
@@ -2,6 +2,8 @@
 
 import CerroImg from '@/app/assets/cerro.svg'
 import System from '@/app/assets/system.svg'
+import Terminal from '@/app/assets/terminal.svg'
+import DesignPessoal from '@/app/assets/designpessoal.svg'
 
 import Image from 'next/image'
 
@@ -10,8 +12,8 @@ export function Designs() {
         <div className='flex md:mt-5 md:pl-52 space-x-5 pl-4 overflow-x-auto pr-2 pb-2'>
             <Image src={CerroImg} width={800} height={800} alt="" className='h-96 rounded-sm w-[1000px]' />
             <Image src={System} width={800} height={800} alt="" className='h-96 rounded-sm w-[1000px]' />
-            <Image src={CerroImg} width={800} height={800} alt="" className='h-96 rounded-sm w-[1000px]' />
-            <Image src={CerroImg} width={800} height={800} alt="" className='h-96 rounded-sm w-[1000px]' />       
+            <Image src={Terminal} width={800} height={800} alt="" className='h-96 rounded-sm w-[1000px]' />
+            <Image src={DesignPessoal} width={800} height={800} alt="" className='h-96 rounded-sm w-[1000px]' />       
         </div>
     
     ) 
