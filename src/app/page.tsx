76a822e0e31bfa9d31fc02362691ae8032b2234c
Pedro Dias <71470442+diaspd@users.noreply.github.com>
2023-07-11 19:31:40 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { ButtonHome } from "./components/ButtonHome";
 import { Designs } from "./components/Designs";
-import { Projects } from "./components/Projects/inex";
+import { Projects } from "./components/Projects";
 
 export default function Home() {
   return (
     <div className="w-full h-screen">
+      <div className="pb-20">
     <main className="flex flex-col p-10 md:px-52 px-2 pl-4 mt-5">
       <h2 className="text-gray-200 text-5xl font-semibold">A sua empresa de tecnologia</h2>
       <p className="text-gray-200 mt-4 text-xl">Precisando de um site para tornar sua empresa mais profissional e   elegante?  <br></br> <strong className="text-gray-200 text-xl">Nos temos a Solução!</strong>
@@ -12,8 +13,6 @@ export default function Home() {
       
       <br />
       <p className="text-gray-200 mt-4 text-xl">Faça seu orçamento <strong>GRÁTIS</strong>, e de um <span className="font-semibold bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-transparent">UPGRADE</span> na sua empresa / negócio.</p>
-
-
     </main>
 
     <ButtonHome />
@@ -27,6 +26,7 @@ export default function Home() {
       Alguns de nossos designs:
     </span>
     <Designs />
+    </div>
     </div>
   )
 }
