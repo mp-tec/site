6736a9f0753140e1c773fe24e3cb1c140c5092be
mp-tec <mptec24@gmail.com>
2023-07-26 20:35:08 -0300

    ci: change svg to png

--- src/app/components/Designs/index.tsx
+++ src/app/components/Designs/index.tsx
@@ -1,15 +1,15 @@
-import CerroImg from '@/app/assets/cerro.svg'
-import System from '@/app/assets/system.svg'
-import Terminal from '@/app/assets/terminal.svg'
-import DesignPessoal from '@/app/assets/designpessoal.svg'
+import CerroImg from '@/app/assets/cerro.png'
+import System from '@/app/assets/system.png'
+import Terminal from '@/app/assets/terminal.png'
+import DesignPessoal from '@/app/assets/designpessoal.png'
 
 import Image from 'next/image'
 
 export function Designs() {
     return (
         <div className='flex md:mt-5 md:pl-52 space-x-5 pl-4 overflow-x-auto pr-2 pb-2'>
-            <Image src={CerroImg} quality={100} priority={true} alt="" className='h-96 rounded-sm w-[1000px]' />
-            <Image src={System} width={600} height={600} alt="" className='h-96 rounded-sm w-[1000px]' />
+            <Image src={CerroImg} alt="" className='h-96 rounded-sm w-[1000px]' />
+            <Image src={System} width={600} height={600} loading="eager" alt="" className='h-96 rounded-sm md:w-[1000px]' />
             <Image src={Terminal} width={600} height={600} alt="" className='h-96 rounded-sm w-[1000px]' />
             <Image src={DesignPessoal} width={600} height={600} alt="" className='h-96 rounded-sm w-[1000px]' />       
         </div>
--- src/app/components/Projects/index.tsx
+++ src/app/components/Projects/index.tsx
@@ -1,15 +1,15 @@
-import BarberImg from '@/app/assets/barber.svg'
-import CoffeeImg from '@/app/assets/coffee.svg'
-import ShopImg from '@/app/assets/shop.svg'
-import BooksImg from '@/app/assets/books.svg'
+import BarberImg from '@/app/assets/barber.png'
+import CoffeeImg from '@/app/assets/coffee.png'
+import ShopImg from '@/app/assets/shop.png'
+import BooksImg from '@/app/assets/books.png'
 
 import Image from 'next/image'
 
 export async function Projects() {
     return (
       <div className='flex md:mt-5 md:pl-52 space-x-5 pl-4 overflow-x-auto pr-2 pb-2 mb-32'>
-          <Image src={ShopImg} width={600} height={600} priority quality={300} alt="" className='h-96 rounded-sm'/>
-          <Image src={BarberImg} width={600} height={600} alt="" className='h-96 rounded-sm'/> 
+          <Image src={ShopImg} width={600} quality={100} priority={true} height={600} alt="" className='h-96 rounded-sm'/>
+          <Image src={BarberImg} width={600} height={600} loading="eager" alt="" className='h-96 rounded-sm'/> 
           <Image src={CoffeeImg} width={600} height={600} alt="" className='h-96 rounded-sm'/>
           <Image src={BooksImg} width={600} height={600} alt="" className='h-96 rounded-sm'/>
       </div>
